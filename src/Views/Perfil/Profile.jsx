1dfc6ec2c4e5b8b0903a1f361ed1026937653959
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2021-11-16 10:47:46 -0300

    [FEAT][Marinho] - primeira parte do front perfil finalizada

--- src/Views/Perfil/Profile.jsx
+++ src/Views/Perfil/Profile.jsx
@@ -14,19 +14,19 @@ export default function Profile() {
                         </div>
                         <div className="textos-desc">
                             <h1 className="nome">nomedousuario</h1>
-                        <h3 className="biografia">biografia</h3>
+                            <h3 className="biografia">biografia aaaaaaaa aaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaa</h3>
                         </div>
-                        
+
                     </div>
                     <div className="destaques-div" >
-                        <div className="destaque-pequeno">
-
+                        <div className="destaque-pequeno" style={{backgroundColor:"#60A848"}}>
+                            <div className="pokemon" />
                         </div>
-                        <div className="destaque-grande">
-
+                        <div className="destaque-grande" style={{backgroundColor:"#F86000"}}>
+                            <div className="pokemon" />
                         </div>
-                        <div className="destaque-pequeno">
-
+                        <div className="destaque-pequeno" style={{backgroundColor:"#94D5FF"}}>
+                            <div className="pokemon" />
                         </div>
                     </div>
                 </div>
